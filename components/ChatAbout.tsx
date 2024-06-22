44d2a4b79744f5ce748e419bcddf90b929eba85d
fuxsociety <musharafz2k3@gmail.com>
2024-06-22 09:54:32 +0530

    completed

---
+++ components/ChatAbout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ChatAbout() {
+	return (
+		<div className="flex-1 flex items-center justify-center">
+			<div className="text-center space-y-5">
+				<h1 className="text-3xl font-bold">Welcome to Chat</h1>
+				<p className="w-96">
+					This is a chat Application. Login with github to use it
+				</p>
+			</div>
+		</div>
+	);
+}
